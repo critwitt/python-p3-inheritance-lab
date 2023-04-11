e6fbda15ec87a4113cdddd7363da37dbdb5d21fa
Chris Witte <christopher.t.witte@gmail.com>
2023-04-11 10:11:40 -0400

    solution

--- lib/teacher.py
+++ lib/teacher.py
@@ -5,6 +5,10 @@ from user import User
 import random
 
 class Teacher(User):
+    
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = ["Chile is a country"]
 
     def teach(self):
-        pass
+        return self.knowledge[random.randint(0, len(self.knowledge) - 1)]
